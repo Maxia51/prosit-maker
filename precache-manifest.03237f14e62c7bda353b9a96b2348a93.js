a92d1e87c1ebf4c6aa36342e64c79098394ead56
maxence <maxence.margaine@gmail.com>
2019-11-06 14:09:07 +0100

    Updates

---
+++ precache-manifest.03237f14e62c7bda353b9a96b2348a93.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f924e4ad97bc42f2bfdbec5e63cf58f",
+    "url": "/prosit-maker/index.html"
+  },
+  {
+    "revision": "c54405d5d9f83dc5db9a",
+    "url": "/prosit-maker/static/css/2.7ff19518.chunk.css"
+  },
+  {
+    "revision": "358067f1acc64b2c0635",
+    "url": "/prosit-maker/static/css/main.67bb3c7a.chunk.css"
+  },
+  {
+    "revision": "c54405d5d9f83dc5db9a",
+    "url": "/prosit-maker/static/js/2.ba9b6900.chunk.js"
+  },
+  {
+    "revision": "358067f1acc64b2c0635",
+    "url": "/prosit-maker/static/js/main.7a7da5a0.chunk.js"
+  },
+  {
+    "revision": "cbdd949785eec3cf62be",
+    "url": "/prosit-maker/static/js/runtime~main.9a6cb12d.js"
+  },
+  {
+    "revision": "7663d0c47292d3c50eb71d008ed8a340",
+    "url": "/prosit-maker/static/media/VERSION.7663d0c4.bin"
+  },
+  {
+    "revision": "a5bc912f13d003ec52b1804e046eba14",
+    "url": "/prosit-maker/static/media/markdown.a5bc912f.md"
+  }
+]);
---
+++ static/media/markdown.a5bc912f.md
@@ -0,0 +1,23 @@
+# {title}
+{Roles}
+
+## Kewords
+{Kewords}
+
+## Context
+{Context}
+
+## Generalization
+{Generalization}
+
+## Constraint
+{Constraint}
+
+## Problematics
+{Problematics}
+
+## Hypothesis
+{Hypothesis}
+
+## Action Plan
+{Actions}
